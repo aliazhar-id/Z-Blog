e5b50696474cb5fa3042c1689149725abad11e7b
aliazhar <aliazhar.idx@gmail.com>
2023-11-13 22:38:35 +0700

    [ ^ ] Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AddonsController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\InterfaceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,7 +22,13 @@ Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login', [AuthController::class, 'actionLogin'])->name('actionLogin');
 
 Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/register', [AuthController::class, 'actionRegister'])->name('actionRegister');
+
+Route::get('/forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot-password');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
 Route::get('/logout', [AuthController::class, 'actionlogout'])->name('logout')->middleware('auth');
 
+Route::get('/interfaces/{groups}/{interface}', [InterfaceController::class, 'index'])->middleware('auth');
+
+Route::get('/addons/{pages}/{addon?}', [AddonsController::class, 'index'])->middleware('auth');
