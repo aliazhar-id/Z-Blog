be0e59f1d7d5d670ed85091fb0e362d75c606b3e
aliazhar <aliazhar.idx@gmail.com>
2023-11-27 11:24:09 +0700

    [ + ] Add initial home page

---
+++ resources/views/blog/page/home.blade.php
@@ -0,0 +1,11 @@
+@extends('blog.layouts.main')
+
+@section('header')
+  @include('blog.partials.header')
+@endsection
+
+@section('content')
+  <div class="row">
+    <div>Ini Home kek nya</div>
+  </div>
+@endsection
