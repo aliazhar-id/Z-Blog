8e5619cf3823370d864bb36f6e245009368fd495
aliazhar <aliazhar.idx@gmail.com>
2023-11-28 04:45:27 +0700

    [ + ] New Navbar

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,3 +5,15 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+document.querySelectorAll('.dropdown-toggle').forEach(item => {
+  item.addEventListener('click', event => {
+ 
+    if(event.target.classList.contains('dropdown-toggle') ){
+      event.target.classList.toggle('toggle-change');
+    }
+    else if(event.target.parentElement.classList.contains('dropdown-toggle')){
+      event.target.parentElement.classList.toggle('toggle-change');
+    }
+  })
+});
--- resources/views/blog/partials/navbar.blade.php
+++ resources/views/blog/partials/navbar.blade.php
@@ -1,20 +1,32 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="/">Z-Blog</a>
+    <a class="navbar-brand" href="{{ route('home') }}">Z-Blog</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
-        class="navbar-toggler-icon"></span></button>
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="{{ route('home') }}">Home</a>
+          <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="{{ route('home') }}" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ Request::is('posts*') ? 'active' : '' }}" href="{{ route('blog') }}">Blog</a>
+          <a class="nav-link {{ Request::is('posts*') ? 'active' : '' }}" href="{{ route('blog') }}"
+            href="#">Blog</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ Request::is('posts*') ? 'about' : '' }}" href="">About</a>
+          <a class="nav-link {{ Request::is('posts*') ? 'about' : '' }}" href="{{ route('about') }}"
+            href="#">About</a>
         </li>
+      </ul>
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 profile-menu">
+        <li class="nav-item">
+          <a href="{{ route('login') }}"
+            class="nav-link btn btn-secondary lh-base{{ Request::is('login') ? 'about' : '' }}">
+            <i class="bi bi-box-arrow-in-right"></i> Login
+          </a>
+        </li>
+
       </ul>
     </div>
   </div>
