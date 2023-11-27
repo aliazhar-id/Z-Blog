6614edb310126c7d1266a5990082821b45c7e16c
aliazhar <aliazhar.idx@gmail.com>
2023-11-27 11:25:38 +0700

    [ ^ ] Update navbar and routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('blog.page.posts', ['title' => 'Post']);
-});
+  return view('blog.page.home', [
+    'title' => 'Home'
+  ]);
+})->name('home');
+
+Route::get('/posts', [PostController::class, 'index'])->name('blog');
