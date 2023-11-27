696030c60a6c4765132e544853c1ca349d6c07af
aliazhar <aliazhar.idx@gmail.com>
2023-11-27 08:19:19 +0700

    [ +^- ] Delete ENTIRELY, Changed 'dashboard-admin to Z-Blog'

--- resources/views/addons/pages/register.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('auth.register')
-
-@section('custom-alert')
-  <div class="alert alert-warning">
-    Ini adalah halaman demo, register tidak dapat dilakukan disini!
-  </div>
-@endsection
