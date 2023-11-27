88587014a99bb3d832e58c83521258ed06d8a47e
aliazhar <aliazhar.idx@gmail.com>
2023-11-27 19:47:33 +0700

    [ ^ ] Fix Navbar active not correct

--- resources/views/blog/partials/navbar.blade.php
+++ resources/views/blog/partials/navbar.blade.php
@@ -6,9 +6,15 @@
         class="navbar-toggler-icon"></span></button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Home</a></li>
-        <li class="nav-item"><a class="nav-link active" aria-current="page" href="{{ route('blog') }}">Blog</a></li>
-        <li class="nav-item"><a class="nav-link" href="">About</a></li>
+        <li class="nav-item">
+          <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="{{ route('home') }}">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ Request::is('posts*') ? 'active' : '' }}" href="{{ route('blog') }}">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ Request::is('posts*') ? 'about' : '' }}" href="">About</a>
+        </li>
       </ul>
     </div>
   </div>
