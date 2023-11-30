b2b1ccaded025c90cc8fec365ac502557b2a8287
aliazhar <aliazhar.idx@gmail.com>
2023-11-30 07:47:53 +0700

    [ +^- ] Add new sidebar item, resize profile in navbar, delete unused code;

--- resources/views/dashboard/partials/sidebar.blade.php
+++ resources/views/dashboard/partials/sidebar.blade.php
@@ -19,109 +19,36 @@
       <span>Dashboard</span></a>
   </li>
 
-  {{-- <!-- Divider -->
-  <hr class="sidebar-divider">
-
-  <!-- Heading -->
-  <div class="sidebar-heading">
-    Interface
-  </div>
-
-  <!-- Nav Item - Pages Collapse Menu -->
-  <li class="nav-item {{ $active == 'components' ? 'active' : '' }}">
-    <a class="nav-link collapsed" href="" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-      aria-controls="collapseTwo">
-      <i class="fas fa-fw fa-cog"></i>
-      <span>Components</span>
-    </a>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-      <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Custom Components:</h6>
-        <a class="collapse-item" href="/interfaces/components/buttons">Buttons</a>
-        <a class="collapse-item" href="/interfaces/components/cards">Cards</a>
-      </div>
-    </div>
-  </li>
-
-  <!-- Nav Item - Utilities Collapse Menu -->
-  <li class="nav-item {{ $active == 'utilities' ? 'active' : '' }}">
-    <a class="nav-link collapsed" href="" data-toggle="collapse" data-target="#collapseUtilities"
-      aria-expanded="true" aria-controls="collapseUtilities">
-      <i class="fas fa-fw fa-wrench"></i>
-      <span>Utilities</span>
-    </a>
-    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-      <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Custom Utilities:</h6>
-        <a class="collapse-item" href="/interfaces/utilities/colors">Colors</a>
-        <a class="collapse-item" href="/interfaces/utilities/borders">Borders</a>
-        <a class="collapse-item" href="/interfaces/utilities/animations">Animations</a>
-        <a class="collapse-item" href="/interfaces/utilities/other">Other</a>
-      </div>
-    </div>
-  </li>
-
   <!-- Divider -->
   <hr class="sidebar-divider">
 
   <!-- Heading -->
   <div class="sidebar-heading">
-    Addons
+    Post
   </div>
-
-  <!-- Nav Item - Pages Collapse Menu -->
-  <li class="nav-item {{ $active == 'pages' ? 'active' : '' }}">
-    <a class="nav-link collapsed" href="" data-toggle="collapse" data-target="#collapsePages"
-      aria-expanded="true" aria-controls="collapsePages">
-      <i class="fas fa-fw fa-folder"></i>
-      <span>Pages</span>
-    </a>
-    <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-      <div class="bg-white py-2 collapse-inner rounded">
-        <h6 class="collapse-header">Login Screens:</h6>
-        <a class="collapse-item" href="/addons/pages/login">Login</a>
-        <a class="collapse-item" href="/addons/pages/register">Register</a>
-        <a class="collapse-item" href="/addons/pages/forgot-password">Forgot Password</a>
-        <div class="collapse-divider"></div>
-        <h6 class="collapse-header">Other Pages:</h6>
-        <a class="collapse-item" href="/addons/pages/404-page">404 Page</a>
-        <a class="collapse-item" href="/addons/pages/blank-page">Blank Page</a>
-      </div>
-    </div>
-  </li>
-
-  <!-- Nav Item - Charts -->
-  <li class="nav-item {{ $active == 'charts' ? 'active' : '' }}">
-    <a class="nav-link" href="/addons/charts">
-      <i class="fas fa-fw fa-chart-area"></i>
-      <span>Charts</span></a>
-  </li>
-
-  <!-- Nav Item - Tables -->
-  <li class="nav-item {{ $active == 'tables' ? 'active' : '' }}">
-    <a class="nav-link" href="/addons/tables">
-      <i class="fas fa-fw fa-table"></i>
-      <span>Tables</span></a>
-  </li> --}}
-
-  <hr class="sidebar-divider">
 
   <!-- Nav Item - My Post -->
   <li class="nav-item {{ Request::is('dashboard/posts') ? 'active' : '' }}">
-    <a class="nav-link pt-0" href="/dashboard/posts">
+    <a class="nav-link pb-2" href="/dashboard/posts">
       <i class="far fa-file-alt"></i>
       <span>My Post</span></a>
   </li>
 
-  {{-- <hr class="sidebar-divider"> --}}
+  <!-- Nav Item - Add Post -->
+  <li class="nav-item {{ Request::is('dashboard/posts/create') ? 'active' : '' }}">
+    <a class="nav-link pt-0" href="/dashboard/posts/create">
+      <i class="far fa-file-alt"></i>
+      <span>Add Post</span></a>
+  </li>
 
+  <!-- Heading -->
   <div class="sidebar-heading">
     Settings
   </div>
 
   <!-- Nav Item - Profile -->
   <li class="nav-item {{ Request::is('profile') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ route('profile') }}">
+    <a class="nav-link" href="/profile">
       <i class="fas fa-fw fa-user"></i>
       <span>Profile</span></a>
   </li>
@@ -133,16 +60,5 @@
   <div class="text-center d-none d-md-inline">
     <button class="rounded-circle border-0" id="sidebarToggle"></button>
   </div>
-
-  <!-- Sidebar Message -->
-  {{-- <div class="sidebar-card d-none d-lg-flex">
-    <img class="sidebar-card-illustration mb-2" src="/img/undraw_rocket.svg" alt="...">
-    <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features,
-      components,
-      and more!</p>
-    <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to
-      Pro!</a>
-  </div> --}}
-
 </ul>
 <!-- End of Sidebar -->
