b955892917779bf124dbeb57286b90e1a4376cf0
aliazhar <aliazhar.idx@gmail.com>
2023-12-03 06:43:59 +0700

    [ + ] Add dismissible session alert into my post tab

--- resources/views/dashboard/posts.blade.php
+++ resources/views/dashboard/posts.blade.php
@@ -21,6 +21,22 @@
     </form>
   </div>
 
+  @if (session('success'))
+    <div class="alert alert-success border-left-success alert-dismissible fade show" role="alert">
+      {{ session('success') }}
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  @elseif(session('error'))
+    <div class="alert alert-warning border-left-warning alert-dismissible fade show" role="alert">
+      {{ session('error') }}
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  @endif
+
   <div class="col-lg-12">
     <a href="/dashboard/posts/create" class="btn btn-primary mb-3 mx-">Create new Post</a>
   </div>
