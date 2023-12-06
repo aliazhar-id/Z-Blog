528e627da7d519e719e7f86debaaa716866f334c
aliazhar <aliazhar.idx@gmail.com>
2023-12-07 05:54:38 +0700

    [ ^ ] Fix path typo

--- resources/views/dashboard/layouts/master.blade.php
+++ resources/views/dashboard/layouts/master.blade.php
@@ -33,13 +33,13 @@
   <link rel="apple-touch-icon" sizes="144x144" href="/assets/apple-icon-144x144.png">
   <link rel="apple-touch-icon" sizes="152x152" href="/assets/apple-icon-152x152.png">
   <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192" href="/asets/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/asets/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="/asets/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/asets/favicon-16x16.png">
-  <link rel="manifest" href="/asets/manifest.json">
+  <link rel="icon" type="image/png" sizes="192x192" href="/assets/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/assets/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
+  <link rel="manifest" href="/assets/manifest.json">
   <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="/asets/ms-icon-144x144.png">
+  <meta name="msapplication-TileImage" content="/assets/ms-icon-144x144.png">
   <meta name="theme-color" content="#ffffff">
 </head>
 
