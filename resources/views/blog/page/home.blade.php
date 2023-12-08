114d796530d8b74bddd2fe5a58fedfc7e053b184
aliazhar <aliazhar.idx@gmail.com>
2023-12-08 20:31:06 +0700

    [ ^ ] Just don't like the tagline :)

--- resources/views/blog/page/home.blade.php
+++ resources/views/blog/page/home.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
       <div class="text-center my-5">
         <h1 class="fw-bolder">Welcome to Z-Blog!</h1>
-        <p class="lead mb-0">Unleash Your Inner Gen Z with Z-Blog</p>
+        <p class="lead mb-0">Share Your Genius: Submit Your Ideas Today!</p>
       </div>
     </div>
   </header>
