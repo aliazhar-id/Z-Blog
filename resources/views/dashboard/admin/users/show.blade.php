01b6afcb4a1efb8589a76d8940932e342cbb3ec6
aliazhar <aliazhar.idx@gmail.com>
2023-12-14 09:54:44 +0700

    [ ^ ] Update admin users show design and add back button

--- resources/views/dashboard/admin/users/show.blade.php
+++ resources/views/dashboard/admin/users/show.blade.php
@@ -22,36 +22,39 @@
 @endphp
 
 @section('content')
-  <div class="row">
-    <div class="col-lg-8 order-lg-1 mx-auto">
-      <div class="card shadow mb-4">
-        <div class="card-header py-3">
-          <h6 class="m-0 font-weight-bold text-primary">User Information</h6>
+  <div class="col-lg-8 order-lg-12 mx-auto">
+    <div class="card shadow mb-2">
+      <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">User Information</h6>
+      </div>
+
+      <div class="card-body">
+        <div class="card-profile-image mt-1 text-center position-relative">
+          <div class="rounded-circle overflow-hidden position-relative mx-auto shadow-lg" style="width: fit-content">
+            <img width="180px" height="180px" id="profile-image-preview"
+              src="{{ $user->image ? asset('storage/' . $user->image) : '/assets/guest.jpeg' }}">
+          </div>
         </div>
 
-        <div class="card-body">
-          <div class="card-profile-image mt-4 text-center position-relative">
-            <div class="rounded-circle overflow-hidden position-relative mx-auto shadow-lg" style="width: fit-content">
-              <img width="180px" height="180px" id="profile-image-preview"
-                src="{{ $user->image ? asset('storage/' . $user->image) : '/assets/guest.jpeg' }}">
+        <div class="p-2 mt-2">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="text-center">
+                <h5 class="font-weight-bold">{{ $user->name }}</h5>
+                <p class="mb-1">{{ '@' . $user->username }}</p>
+                <p>{{ $user->email }}</p>
+                <p>{{ ucfirst($user->role) }}</p>
+              </div>
+            </div>
+            <div class="col text-center">
+              {{ $postCount }} Post | {{ $totalClick }} Click | {{ $grade }} Grade
             </div>
           </div>
+        </div>
 
-          <div class="p-2 mt-4">
-            <div class="row">
-              <div class="col-lg-12">
-                <div class="text-center">
-                  <h5 class="font-weight-bold">{{ $user->name }}</h5>
-                  <p class="mb-1">{{ '@' . $user->username }}</p>
-                  <p>{{ $user->email }}</p>
-                  <p>{{ ucfirst($user->role) }}</p>
-                </div>
-              </div>
-              <div class="col text-center">
-                {{ $postCount }} Post | {{ $totalClick }} Click | {{ $grade }} Grade
-              </div>
-            </div>
-          </div>
+        <!-- Button -->
+        <div class="col text-center mb-2">
+          <a href="{{ route('users.index') }}" class="btn btn-primary mt-3 ">Back</a>
         </div>
       </div>
     </div>
